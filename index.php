f574afdfb990f75a72b0551c21abe8a95dbf2499
Jemyll Ramoya <150319788+XxjhemxX@users.noreply.github.com>
2024-06-28 22:19:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 $dbhost='127.0.0.1';
 $dbuser='root';
 $dbpass='';
@@ -10,22 +11,7 @@ try {
   die("Error somewhere");
 }
 
-?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>Log-In</title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-
-<?php
-
+// print_r($_POST);
 if (isset($_POST['username'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
@@ -36,24 +22,70 @@ if (isset($_POST['username'])) {
 
   $row = mysqli_fetch_assoc($result);
 
-  // username found
   if(isset($row)) {
     // pupunta sa next page
     header('location: post.php');
   } 
-
 }
 
 ?>
 
-  <!-- <img src="images/bg.jpg"> -->
-  <form class="box" action="index.php" method="post">
-    <h1>LOG-IN</h1>
-    <input type="text" placeholder="Username" name="username">
-    <input type="text" style="-webkit-text-security: circle" placeholder="Enter Password" required name="password" />
-    <input type="submit" placeholder="Login">
-  </form>
- 
-</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Final Project</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">Jhem<span>Bellemente</h2>
+            </div>
 
+            <div class="menu">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </div>
+
+
+        </div> 
+        <div class="content">
+            <h1>Web App<br><span>Development 2</span> <br>BSIS-2</h1><br><br>
+            <p class="par">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt neque 
+                 expedita <br> atque eveniet quis nesciunt. Quos nulla vero consequuntur, fugit nemo ad <br>
+                 delectus a quae totam ipsa illum minus laudantium?</p>
+
+                <form class="form" action="index.php" method="post">
+                    <h2>Login Here</h2>
+                    <input type="text" name="username" placeholder="username">
+                    <input type="password" name="password" placeholder="password">
+                    <input type="submit" value="Submit" class="submit-btn">
+
+                    <p class="link">Don't have an account<br>
+                    <a href="#">Sign up </a> here</a></p>
+                    <p class="liw">Log in with</p>
+
+                    <div class="icons">
+                        <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
+                    </div>
+
+                </form>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+</body>
 </html>
+
+
+
